050c96c6e19c2ed4be29ab3afa1786273247e5d4
Ayushk4 <ayushk4@gmail.com>
2021-01-09 00:28:38 -0800

    boilerplate + Trainer + common + data build

---
+++ common/callbacks/epoch_end_callbacks/checkpoint.py
@@ -0,0 +1,36 @@
+#----------------------------------------
+#--------- OS related imports -----------
+#----------------------------------------
+import os
+from pathlib import Path
+
+#----------------------------------------
+#--------- Torch related imports --------
+#----------------------------------------
+import torch
+
+class Checkpoint():
+    def __init__(self, config, val_metrics):
+        self.config = config
+        self.val_metrics = val_metrics
+        self.save_path = self.set_up_logging_dir()
+
+    def set_up_logging_dir(self):
+        ckpt_dir_path = os.path.join('ckpts', self.config.VERSION)
+        Path(ckpt_dir_path).mkdir(parents=True, exist_ok=True)
+        return ckpt_dir_path
+
+    def __call__(self, rank=0, epoch=0, net=None, optimizer=None, save_all_ckpts=False, **kwargs):
+        # save the current epoch metrics
+        curr_save_info = {
+                'net_state_dict':net.state_dict(),
+                'optim_state_dict':optimizer.state_dict()
+                }
+
+        if save_all_ckpts and rank == 0:
+            torch.save(curr_save_info, os.path.join(self.save_path, f'epoch_{epoch}.pth'))
+
+        if self.val_metrics.all_metrics['val_accuracy'].updated_best_val and rank == 0:
+            print("Saving new best model...")
+            torch.save(curr_save_info, os.path.join(self.save_path, f'best.pth'))
+            print("Done!!")
---
+++ common/metrics/train_metrics.py
@@ -0,0 +1,33 @@
+#----------------------------------------
+#--------- Torch related imports --------
+#----------------------------------------
+import torch
+import torch.distributed as distributed
+
+#----------------------------------------
+#--------- Local Imports ----------------
+#----------------------------------------
+from common.metrics.basic_metrics import TrainAccuracyMetric, TrainLossMetric
+
+class TrainMetrics():
+    def __init__(self, config, allreduce=False):
+        self.allreduce = allreduce
+
+    def reset(self):
+        # To reset the metrics after every epoch
+        # Create a single dict containing all the metrics
+        self.all_metrics = {}
+
+    def store(self, metric_name, metric_value, metric_type='Accuracy'):
+
+        if metric_name not in self.all_metrics:
+            # initialize a new metric
+            self.all_metrics[metric_name] = eval(f'Train{metric_type}Metric')(metric_value, allreduce=self.allreduce)
+        else:
+            # update the metric
+            self.all_metrics[metric_name].update(metric_value)
+
+    def wandb_log(self, step):
+
+        for metric_name, metric in self.all_metrics.items(): 
+            metric.wandb_log(metric_name, step)
