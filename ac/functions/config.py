0242b3c60e03850e508d91ba522e5ddd0fe51604
Ayushk4 <ayushk4@gmail.com>
2021-01-16 00:16:46 -0800

    Write Config.py for AC

--- ac/functions/config.py
+++ ac/functions/config.py
@@ -7,7 +7,86 @@ config = _C
 # ------------------------------------------------------------------------------------- #
 # Common options
 # ------------------------------------------------------------------------------------- #
-_C.RNG_SEED = -1
+_C.RNG_SEED = 5
+
+_C.NUM_WORKERS_PER_GPU = 1
+_C.GPUS = [0, 1, 2, 3, 4, 5, 6, 7]
+
+_C.OUTPUT_PATH = ""
+
+_C.MODEL = ""
+
+
+# ------------------------------------------------------------------------------------- #
+# Train options
+# ------------------------------------------------------------------------------------- #
+_C.TRAIN = edict()
+_C.TRAIN.BATCH_SIZE = 
+_C.TRAIN.SHUFFLE = True
+_C.TRAIN.LR = -10
+_C.TRAIN.MOMENTUM = 0.9
+_C.TRAIN.WEIGHT_DECAY = 0.01
+_C.TRAIN.OPTIMIZER = 'AdamW'
+_C.TRAIN.BEGIN_EPOCH = 0
+_C.TRAIN.END_EPOCH = 10
+
+
+# ------------------------------------------------------------------------------------- #
+# Val options
+# ------------------------------------------------------------------------------------- #
+_C.VAL = edict()
+_C.VAL.BATCH_SIZE = 
+_C.VAL.SHUFFLE = False
+
+
+# ------------------------------------------------------------------------------------- #
+# Dataset options
+# ------------------------------------------------------------------------------------- #
+_C.DATASET = edict()
+_C.DATASET.DATA_PATH = ""
+
+_C.DATASET.CLASS_TO_INDEX_FILE = "" # Relative to DATASET.DATA_PATH
+_C.DATASET.TRAIN_ANNOTATIONS_PATH = "" # Relative to DATASET.DATA_PATH
+_C.DATASET.VAL_ANNOTATIONS_PATH = "" # Relative to DATASET.DATA_PATH
+
+_C.DATASET.TRAIN_VIDEO_RESIZED_PATH = "" # Relative to DATASET.DATA_PATH
+_C.DATASET.VAL_VIDEO_RESIZED_PATH = "" # Relative to DATASET.DATA_PATH
+
+_C.DATASET.TOY = False
+_C.DATASET.TOY_SAMPLES = 10
+
+_C.DATASET.SAMPLING_STRIDE = 2
+
+_C.DATASET.TRAIN_SPLIT = "train"
+_C.DATASET.VAL_SPLIT = "val"
+
+
+# ------------------------------------------------------------------------------------- #
+# Network options
+# ------------------------------------------------------------------------------------- #
+
+_C.NETWORK = edict()
+_C.NETWORK.BACKBONE = ""
+_C.NETWORK.BACKBONE_LOAD_PRETRAINED = True
+
+_C.NETWORK.TEMPORAL_MLP_DIMS = 512
+_C.NETWORK.TEMPORAL_MLP_ACTIVATION = "" # The code will prepend `torch.nn.` and do eval over the string.
+
+_C.NETWORK.TRANSFORMER_DIMS = 512
+_C.NETWORK.TRANSFORMER_HEADS = 8
+_C.NETWORK.TRANSFORMER_ENCODER_CNT = 8
+_C.NETWORK.TRANSFORMER_DROPOUT = 0.1
+_C.NETWORK.TRANSFORMER_FEEDFORWARD_DIMS = 2048
+
+_C.NETWORK.POSITIONAL_DROPOUT = 0.1
+_C.NETWORK.NUM_CLASSES = 700
+
+_C.NETWORK.PARTIAL_PRETRAIN = False
+
+
+# ------------------------------------------------------------------------------------- #
+# Update Config
+# ------------------------------------------------------------------------------------- #
 
 
 def update_config(config_file):
