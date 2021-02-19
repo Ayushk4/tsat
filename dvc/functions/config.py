bff6044b95f8ecee62e97cb33a3cdcc262648f77
Ayushk4 <ayushk4@gmail.com>
2021-02-19 01:39:47 -0800

    config for dvc done

--- dvc/functions/config.py
+++ dvc/functions/config.py
@@ -10,6 +10,29 @@ config = _C
 _C.RNG_SEED = -1
 
 
+# ------------------------------------------------------------------------------------- #
+# Common options
+# ------------------------------------------------------------------------------------- #
+_C.DATASET = edict()
+_C.DATASET.ROOT_PATH = "/data/datasets/"
+_C.DATASET.DATA_PATH = "ActionGenome/"
+_C.DATASET.FRAMES_PATH = "dataset/ag/frames"
+_C.DATASET.TRAIN_ANNOTATIONS_PATH = "/workspace/ayushk4/Create_stdvc/cache/idxed_train.json"
+_C.DATASET.VAL_ANNOTATIONS_PATH = "/workspace/ayushk4/Create_stdvc/cache/idxed_val.json"
+_C.DATASET.DATASET_NAME = "unnamed_dataset"
+
+_C.DATASET.TOY = False
+_C.DATASET.TOY_SAMPLES = 10
+
+_C.DATASET.SAMPLING_STRIDE = 2
+_C.DATASET.SAMPLING_COUNT = 8
+
+_C.DATASET.TRAIN_SPLIT = "train"
+_C.DATASET.VAL_SPLIT = "val"
+
+
+
+
 def update_config(config_file):
     with open(config_file) as f:
         exp_config = edict(yaml.load(f))
@@ -22,7 +45,7 @@ def update_config(config_file):
                                 config[k][vk] = tuple(float(s) for s in vv.split(','))
                             elif vk == 'LOSS_LOGGERS':
                                 config[k][vk] = [tuple(str(s) for s in vvi.split(',')) for vvi in vv]
-                             else:
+                            else:
                                 config[k][vk] = vv
                         else:
                             raise ValueError("key {}.{} not in config.py".format(k, vk))
